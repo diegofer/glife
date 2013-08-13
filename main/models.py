d87f206bb9e2c5d01030f127adca0efb0b12b8af
xyiyox <yiyo1224@hotmail.com>
2013-08-13 13:21:49 -0500

    Introducidas los modelos basicos y algunas de sus relaciones para ser sometidas a evaluacion

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Persona(models.User):
+
+	nombre 	= models.CharField(max_length=50)
+	apellido = models.CharField(max_length=50)
+	telefono = models.IntegerField()
+	email = models.CharField(max_length=100)
+	fecha_nacimiento = models.DateField()
+	
+class Lider(Persona):
+
+	persona = models.ForeignKey(Persona)
+
+	
+class Miembro(Persona):
+
+	lider = models.OneToOneField(Lider)
+	
+class Colider(Miembro):
+	
+class Grupo(models.Model):
+
+	lider = models.OneToOneField(Lider)
+
+
+class Grupograma(models.Model):
+
+    
+		
+		
+
+		
+		
+		
