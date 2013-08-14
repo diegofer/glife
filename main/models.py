5208e0c69f7acbf4e428335406bd22848253d24f
xyiyox <yiyo1224@hotmail.com>
2013-08-13 23:48:00 -0500

    Primera actualizacion al esquema basico de modelos

--- main/models.py
+++ main/models.py
@@ -12,17 +12,26 @@ class Persona(models.User):
 class Lider(Persona):
 
 	persona = models.ForeignKey(Persona)
+	grupos = models.ForeignKey(Grupo)
+	colideres = models.ForeignKey(Colider)
 
 	
 class Miembro(Persona):
 
 	lider = models.OneToOneField(Lider)
+	grupo = models.OneToOneField(Grupo)
 	
 class Colider(Miembro):
-	
+	"""
+	Posee un lider y un grupo asignado
+	al heredar del modelo Miembro
+	"""
+
 class Grupo(models.Model):
 
 	lider = models.OneToOneField(Lider)
+	colideres = models.ForeignKey(Colider)
+	miembros = models.ForeignKey(Miembro)
 
 
 class Grupograma(models.Model):
